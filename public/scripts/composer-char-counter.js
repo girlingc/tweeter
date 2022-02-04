d78f5ffb31dd6ba475b65f5ec50e69c5220533ae
Christian Girling <girling.christian@gmail.com>
2022-02-03 23:38:35 -0800

    Added error message

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-  // Disable button on initial load
-  $('#tweet-button').prop('disabled', true).addClass('disabled')
 
+$(document).ready(function() {
+  // Disable button and error message on initial load
+  $('#tweet-button').prop('disabled', true).addClass('disabled')
+  $('.err-msg').hide();
   // Character counter and handling for invalid input
   $("#tweet-text").on("input", function(e) {
     const tweetLength = this.value.length;
@@ -10,11 +11,13 @@ $(document).ready(function() {
     if ((140 - tweetLength) < 0) {
       charCounter.addClass('counter-over');
       $('#tweet-button').prop('disabled', true).addClass('disabled');
+      $(".err-msg").text("You wrote over 140 characters!").fadeIn();
     } 
     else if (tweetLength === 0 || this.value.trim().length === 0) {
       $('#tweet-button').prop('disabled', true).addClass('disabled');
     } 
     else {
+      $(".err-msg").fadeOut();
       charCounter.removeClass('counter-over');
       $('#tweet-button').prop('disabled', false).removeClass('disabled');
     }
